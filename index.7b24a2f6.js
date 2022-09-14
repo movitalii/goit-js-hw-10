f11011016f572e09648a682c640910ae29d64018
movitalii <103452623+movitalii@users.noreply.github.com>
2022-09-14 18:50:43 +0000

    Deploying to gh-pages from @ movitalii/goit-js-hw-10@37256f0116de83312d0770a89ff80c1df98f4f9f 🚀

---
+++ index.7b24a2f6.js
@@ -0,0 +1,2 @@
+fetch("https://restcountries.com/v3.1/name/ukraine").then((function(n){return n.json()})).then((function(n){console.log(n)})).catch((function(n){console.log(n)}));
+//# sourceMappingURL=index.7b24a2f6.js.map
