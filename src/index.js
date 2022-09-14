37256f0116de83312d0770a89ff80c1df98f4f9f
Vitalii Mordovets <vitalii.mordovets@gmail.com>
2022-09-14 21:49:57 +0300

    fetch URL

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 import './css/styles.css';
 
-const DEBOUNCE_DELAY = 300;
+const DEBOUNCE_DELAY = 300; 
+
+
+fetch('https://restcountries.com/v3.1/name/ukraine')
+    .then(response => {
+        return response.json();
+    })
+    .then(country => {        
+        console.log(country);
+//             return `<div class="card">
+//     <div class="card-header">
+//         <img src="${country.flags.svg}" alt="${country.name.official}">
+//         <h2 class="card-title">${country.name.official}</h2>
+//     </div>
+//     <div class="card-body">
+//         <p class="card-text">Capital: ${country.capital}</p>
+//         <p class="card-text">Population: ${country.population}</p>
+//         <p class="card-text">Languages: ${country.languages}</p>
+//     </div>
+// </div>`
+    })
+    .catch(error => {
+        console.log(error);
+    });
+
+
