66a4197aaf876ac5e0914fbaab19a7fa0feed777
movitalii <103452623+movitalii@users.noreply.github.com>
2022-09-17 13:34:21 +0000

    Deploying to gh-pages from @ movitalii/goit-js-hw-10@e56b9415b33b077ea606b0d8819bfd0843a7a95c 🚀

--- index.7b24a2f6.js
+++
@@ -1,2 +0,0 @@
-fetch("https://restcountries.com/v3.1/name/ukraine").then((function(n){return n.json()})).then((function(n){console.log(n)})).catch((function(n){console.log(n)}));
-//# sourceMappingURL=index.7b24a2f6.js.map
--- index.fc552ae3.js
+++
@@ -1,2 +0,0 @@
-fetch("https://restcountries.com/v3.1/name/ukraine").then((e=>e.json())).then((e=>{console.log(e)})).catch((e=>{console.log(e)}));
-//# sourceMappingURL=index.fc552ae3.js.map
