e56b9415b33b077ea606b0d8819bfd0843a7a95c
Vitalii Mordovets <vitalii.mordovets@gmail.com>
2022-09-17 16:33:30 +0300

    render markup

--- src/index.js
+++ src/index.js
@@ -1,28 +1,49 @@
 import './css/styles.css';
+const DEBOUNCE_DELAY = 300;
+const divEl = document.querySelector("div");
+const ulvEl = document.querySelector("ul");
+const inputEl = document.querySelector("input");
 
-const DEBOUNCE_DELAY = 300; 
+inputEl.addEventListener('input', onInput)
+
+function onInput(event) {
+    const countryName = event.currentTarget.value;
+    fetchCountries(countryName)    
+        .then((country) => renderCard(country))
+        .catch((error) => console.log(error));
+}
+
+function fetchCountries(name) {
+    return fetch(`https://restcountries.com/v3.1/name/${name}?fields=flags,name,capital,population,languages`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(response.status);
+            }
+            return response.json();
+        });
+}
+        
+      
+function renderCard(country) { 
+
+        return `<div class="card">
+    <div class="card-header">
+        <img src="${country.flags.svg}" alt="${country.name.official}">
+        <h2 class="card-title">${country.name.official}</h2>
+    </div>
+    <div class="card-body">
+        <p class="card-text">Capital: ${country.capital}</p>
+        <p class="card-text">Population: ${country.population}</p>
+        <p class="card-text">Languages: ${country.languages}</p>
+    </div>
+</div>`
+}            
+                
 
 
-fetch('https://restcountries.com/v3.1/name/ukraine')
-    .then(response => {
-        return response.json();
-    })
-    .then(country => {        
-        console.log(country);
-//             return `<div class="card">
-//     <div class="card-header">
-//         <img src="${country.flags.svg}" alt="${country.name.official}">
-//         <h2 class="card-title">${country.name.official}</h2>
-//     </div>
-//     <div class="card-body">
-//         <p class="card-text">Capital: ${country.capital}</p>
-//         <p class="card-text">Population: ${country.population}</p>
-//         <p class="card-text">Languages: ${country.languages}</p>
-//     </div>
-// </div>`
-    })
-    .catch(error => {
-        console.log(error);
-    });
+
+
+
+
 
 
