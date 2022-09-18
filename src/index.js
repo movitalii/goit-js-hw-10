be3ebe43426f8f8c1a87ac3dd01f731609b220ab
Vitalii Mordovets <vitalii.mordovets@gmail.com>
2022-09-18 20:56:24 +0300

    hw-10 to check

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './css/styles.css';
 import debounce from 'lodash.debounce';
 import Notiflix from 'notiflix';
+import { fetchCountries }  from './fetchCountries';
 const DEBOUNCE_DELAY = 300;
 const divEl = document.querySelector("div");
 const ulvEl = document.querySelector("ul");
@@ -13,51 +14,54 @@ function onInput(event) {
     if (countryName === '') {
         clearCountryCard();
         clearCountriesList();
-    } else if(countryName !== '') {
+        inputEl.classList.remove("none-cards");
+        inputEl.classList.remove("one-card");
+        inputEl.classList.remove("several-cards");
+    } else if(countryName !== ''){
         fetchCountries(countryName)    
-        .then(renderCountryList)
-        .catch(onInputError);
-    }
-        
-     
+        .then(renderCountries)
+        .catch(onInputError); 
+    }            
 }
-
-function fetchCountries(name) {
-    return fetch(`https://restcountries.com/v3.1/name/${name}?fields=flags,name,capital,population,languages`)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(response.status);
-            }
-            return response.json();
-        });
-}
- 
       
-function renderCountryList(countries) {
-    if (countries.length > 1 && countries.length < 10) {
-        clearCountryCard();
+function renderCountries(countries) {
+    if (countries.length >= 2 && countries.length <= 10) {
+        renderCountriesList(countries);
+    } else if(countries.length === 1){
+        renderCountryCard(countries);
+    } else {    
+        lotsOfCoutriesCards();
+    }            
+}  
+
+function renderCountriesList(countries) {
+    inputEl.classList.remove("none-cards");
+    inputEl.classList.remove("one-card");
+    inputEl.classList.remove("several-cards");
+    clearCountryCard();
         const markup = countries
     .map((country) => {
       return `
           <li class ="country-item">
-            <img class ="country-img" src="${country.flags.svg}" alt="${country.name.official}" width="50">
+            <img class ="country-img" src="${country.flags.svg}" alt="${country.name.official}" width="200">
             <p class="country-name">${country.name.official}</p>
           </li>
       `;
     })
     .join("");
-    ulvEl.innerHTML = markup; 
-    } else if (countries.length > 10) {
-        clearCountryCard();
-        clearCountriesList();
-        Notiflix.Notify.failure("Too many matches found. Please enter a more specific name.");
-    } else {
-        clearCountriesList();
+    ulvEl.innerHTML = markup;
+}
+ 
+function renderCountryCard(countries) {
+    inputEl.classList.remove("none-cards");
+    inputEl.classList.add("one-card");
+    inputEl.classList.remove("several-cards");
+    clearCountriesList();
         const markupForOne = countries
         .map((country) => {
             return `<div class="card">
     <div class="card-header">
-        <img class ="country-img" src="${country.flags.svg}" alt="${country.name.official}" width="50">
+        <img class ="country-img" src="${country.flags.svg}" alt="${country.name.official}" width="200">
         <h2 class="card-title">${country.name.official}</h2>
     </div>
     <div class="card-body">
@@ -69,8 +73,24 @@ function renderCountryList(countries) {
     })
     .join("");
     divEl.innerHTML = markupForOne;
-    }           
-}  
+}
+
+function lotsOfCoutriesCards() {
+    inputEl.classList.remove("none-cards");
+    inputEl.classList.add("several-cards");
+    clearCountryCard();
+    clearCountriesList();
+    Notiflix.Notify.info("Too many matches found. Please enter a more specific name.");
+}
+
+function onInputError() {
+    inputEl.classList.add("none-cards");
+    inputEl.classList.remove("several-cards");
+    inputEl.classList.remove("one-card");
+    Notiflix.Notify.failure("Oops, there is no country with that name");
+    clearCountriesList();
+    clearCountryCard();  
+}
 
 function clearCountriesList() {
     ulvEl.innerHTML = '';
@@ -80,11 +100,6 @@ function clearCountryCard() {
     divEl.innerHTML = '';
 }
 
-function onInputError() {
-    Notiflix.Notify.failure("Oops, there is no country with that name");
-    clearCountriesList;
-    clearCountryCard;
-}
 
                 
 
